5eb342e9f930d20387cbbc2f8acdc13f46f6e18c
byungkyu-ju <noel1227@gmail.com>
2020-06-12 16:01:35 +0900

    init

---
+++ src/config/site.config.js
@@ -0,0 +1,11 @@
+export default {
+  siteName: 'dashboard',
+  siteIcon: 'ion-flash',
+  footerText: `dashboard @ ${new Date().getFullYear()} Created by byungkyu-ju`,
+  enableAnimatedRoute: false,
+  apiUrl: 'http://mypage.com/api/',
+  google: {
+    analyticsKey: 'UA-xxxxxxxxx-1',
+  },
+  dashboard: '/dashboard',
+};
---
+++ src/containers/Sidebar/options.js
@@ -0,0 +1,18 @@
+const options = [
+  {
+    key: 'blankPage',
+    label: 'sidebar.blankPage',
+    leftIcon: 'ion-document',
+  },
+  {
+    key: 'authCheck',
+    label: 'sidebar.authCheck',
+    leftIcon: 'ion-document',
+  },
+  {
+    key: 'blankPage2',
+    label: 'sidebar.blankPage2',
+    leftIcon: 'ion-document',
+  },
+];
+export default options;
